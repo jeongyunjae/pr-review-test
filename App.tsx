719200a4c6bdf7543ebc5238aa420044c8fb878d
jeongyunjae <yunjae2295@soongsil.ac.kr>
2025-01-14 11:08:24 +0900

    wip

--- App.tsx
+++ App.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
 export default function App() {
-  return <div>App바1111112113어</div>
+  return (
+    <>
+      <div>App바1111112113어</div>
+      <div>qw</div>
+    </>
+  )
 }
